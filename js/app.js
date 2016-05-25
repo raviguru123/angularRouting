e45e24de38331f596795a3f223ab7ac88c08ca64
raviguru123 <raviguruiitr@gmail.com>
2016-05-25 23:54:57 +0530

    nesting routing add

--- js/app.js
+++ js/app.js
@@ -3,27 +3,35 @@ var app=angular.module("myapp",["ui.router"]);
 app.config(function($stateProvider, $urlRouterProvider){
 
 	$urlRouterProvider.otherwise("/state1");
-
-	$stateProvider.state("state1",{
+var state1={
 		url:"/state1",
 		views:{
 			"viewA":{templateUrl:"state1.html"},
 			"viewB":{templateUrl:"state2.html"}
 		}
-		
-	}).state("state1.list",{
-		url:"state1.list",
-		templateUrl:"state1.list.html"
+	}
+	$stateProvider.state("state1",state1).state("state1.list1",{
+		url:".list1",
+		views:{"list1View":{templateUrl:"state1.list.html"}}
+	}).state("state1.list2",{
+		url:".list2",
+		views:{"list2View":{templateUrl:"state2.list.html"}}
 	}).
 	state("state2",{
 		url:"/state2",
 		views:{
-			"viewA":{templateUrl:"state1.html"},
-			"viewB":{templateUrl:"state2.html"}
+			"viewB":{templateUrl:"state1.html"},
+			"viewA":{templateUrl:"state2.html"}
 		}
 	}).
-	state("state2.list",{
-		url:"/stat2.list",
-		templateUrl:"state2.list.html"
+	state("state2.list1",{
+		url:".list1",
+		views:{"list1View":{templateUrl:"state1.list.html"}}
+	}).
+	state("state2.list2",{
+		url:".list2",
+		views:{"list2View":{templateUrl:"state2.list.html"},
+	}
 	});
-});
+});;
+
